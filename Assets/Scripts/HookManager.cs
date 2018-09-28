e84218d42745e30b40602e63129c9be72b31da3c
唐卓 <tangz@yomob.com>
2018-09-28 19:52:39 +0800

    1.0

---
+++ Assets/Scripts/HookManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class HookManager : MonoBehaviour {
+	LineRenderer lineRenderer;
+	Transform hook;
+	Transform rightHand;
+
+	Vector3 startPos;
+	Vector3 endPos;
+
+	bool isDoRotate = false;
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+		hook = GameObject.FindGameObjectWithTag ("hook").transform;
+		rightHand = GameObject.FindGameObjectWithTag ("RightHand").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NormalUpdate ();
+	}
+
+	void ThrowUpdate(){
+		
+	}
+
+	void NormalUpdate(){
+		hook.RotateAround (rightHand.position, Vector3.left, 10);
+		//hook.position = new Vector3(rightHand.position.x,hook.position.y,hook.position.z);
+		//hook.parent.rotation = rightHand.rotation;
+
+		startPos = rightHand.position;
+		endPos = hook.position;
+		lineRenderer.positionCount = 2;
+		lineRenderer.SetPosition (0, startPos);
+		lineRenderer.SetPosition (1, endPos);
+	}
+}
