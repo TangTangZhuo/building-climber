9dc173f9ed00086c5b118f98d3a7ab85ff7312e7
唐卓 <tangz@yomob.com>
2018-09-29 19:33:01 +0800

    增加绳子的长短变化

--- Assets/Anima2DTest/Scripts/ThrowRope.cs
+++ Assets/Anima2DTest/Scripts/ThrowRope.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ThrowRope : MonoBehaviour {
 
+	public Transform[] bones;
+	public float speed = 1;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,43 @@ public class ThrowRope : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.Q)) {
+			ChangeLength (false);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			ChangeLength (true);
+		}
+	}
+
+	void CutLength(){
+		for (int i = bones.Length - 1; i >= 1; i--) {
+			
+				//bones [i].position = Vector3.Lerp (bones [i].position, bones [i - 1].position, speed * Time.deltaTime);
+			Vector3 dir = bones[i-1].position-bones[i].position;
+			bones [i].position = Vector3.Lerp (bones [i].position, bones [i].position + dir, speed * Time.deltaTime);
+		}
+	}
+
+	void AddLength(){
+		for (int i = bones.Length-1; i >= 1; i--) {			
+			//Vector3 dir = bones[i+1].position-bones[i].position;
+			//bones [i].position = Vector3.Lerp (bones [i].position, bones [i + 1].position + dir, speed * Time.deltaTime);
+			Vector3 dir = bones[i].position-bones[i-1].position;
+			bones [i].position = Vector3.Lerp (bones [i].position, bones [i].position + dir, speed * Time.deltaTime);
+		}
+	}
+
+	void ChangeLength(bool state){
+		for (int i = bones.Length - 1; i >= 1; i--) {
+			Vector3 dir = Vector3.one;
+			if (state == true) {
+				dir = bones [i - 1].position - bones [i].position;
+			} else {
+				dir = bones [i].position - bones [i - 1].position;
+			}
+			bones [i].position = Vector3.Lerp (bones [i].position, bones [i].position + dir, speed * Time.deltaTime);
+		}
+
+
 	}
 }
