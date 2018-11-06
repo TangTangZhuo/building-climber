6464e785fd6a07470d6f9eff24c2453e3b38cf3d
唐卓 <tangz@yomob.com>
2018-11-06 19:56:30 +0800

    加入飞翔金币

---
+++ Assets/TouchSky/FlyGold/Scripts/GoldCollect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GoldCollect : MonoBehaviour {
+	Transform goldImage;
+
+	void Start () {
+		Invoke ("GetGoldImage", 0.5f);
+	}
+
+	void GetGoldImage(){
+		if (GameObject.FindGameObjectWithTag ("goldImage")) {
+			goldImage = GameObject.FindGameObjectWithTag ("goldImage").transform;
+		}else{
+			Invoke ("DestroyImage", 1f);
+		}
+	}
+
+	void DestroyImage(){
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		
+		if (col.tag == "goldCollider") {
+			goldImage.DOScale (new Vector3 (1.2f, 1.2f, 1.2f), 0.2f).OnComplete(()=>{
+				goldImage.DOScale (1f, 0.2f);
+				Destroy(gameObject);
+			});
+		}
+
+	}
+}
