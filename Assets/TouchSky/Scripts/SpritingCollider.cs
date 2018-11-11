0e9b19ec249c54d12e9dd3e7c8f6e6a165afcfe0
唐卓 <tangz@yomob.com>
2018-11-12 01:43:30 +0800

    修改爆炸特效的跟随方式

--- Assets/TouchSky/Scripts/SpritingCollider.cs
+++ Assets/TouchSky/Scripts/SpritingCollider.cs
@@ -6,11 +6,11 @@ public class SpritingCollider : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.tag == "deadcloud") {
-			Instantiate (ParticleManager.Instance.particle_rocketDead, transform.position, transform.rotation).transform.parent = transform;
+			Instantiate (ParticleManager.Instance.particle_rocketDead, transform.position, transform.rotation);
 			Destroy (coll.gameObject);
 		}
 		if (coll.tag == "rocket") {
-			Instantiate (ParticleManager.Instance.particle_rocketDead, transform.position, transform.rotation).transform.parent = transform;
+			Instantiate (ParticleManager.Instance.particle_rocketDead, transform.position, transform.rotation);
 			Destroy (coll.transform.parent.parent.gameObject);
 		}
 	}
